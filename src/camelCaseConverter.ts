4a5d6c79656ba42698a6e2777bc724e5f2454628
Evelin Virunurm <evelinviru@gmail.com>
2025-02-19 08:07:45 +0100

    Created tests and solved the kata.

--- src/camelCaseConverter.ts
+++ src/camelCaseConverter.ts
@@ -1,3 +1,21 @@
-export function CamelCaseConverter(name: string) {
-    // Kata logic will be here
+export function CamelCaseConverter(text: string): string {
+    const separatorCharacters = ['-', '_'];
+
+    const getWordsFromString = (text: string): string[] => {
+        const space = ' ';
+        return text
+            .replace(new RegExp(separatorCharacters.join('|'), 'g'), space)
+            .split(space)
+    }
+
+    const capitalizeWords = (words: string[]): string[] => {
+       return words.map((word) => {
+           if (word.length === 0) return word;
+           return word[0].toUpperCase() + word.slice(1);
+       })
+    }
+
+    const words = getWordsFromString(text)
+    const capitalizedWords = capitalizeWords(words)
+    return capitalizedWords.join('');
 }
--- test/camelCaseConverter.test.ts
+++ test/camelCaseConverter.test.ts
@@ -1,5 +1,27 @@
 import { CamelCaseConverter } from '../src/camelCaseConverter';
 
 describe('camelCaseConverter', () => {
-    // Tests will be here
+    it('should return the same string if it has no text', () => {
+        expect(CamelCaseConverter('⇒')).toBe('⇒');
+    })
+
+    it('should return the same string if capitalized', () => {
+        expect(CamelCaseConverter('Foo')).toBe('Foo');
+    })
+
+    it('should remove spaces if capitalized and separated by spaces', () => {
+        expect(CamelCaseConverter('Foo Bar')).toBe('FooBar');
+    })
+
+    it('should return join words if capitalized and separated by hyphens', () => {
+        expect(CamelCaseConverter('Foo_Bar-Foo')).toBe('FooBarFoo');
+    })
+
+    it('should return a word with the first letter capitalized', () => {
+        expect(CamelCaseConverter('foo')).toBe('Foo');
+    })
+
+    it('should return words in uppercase and joined if lowercase', () => {
+        expect(CamelCaseConverter('foo_bar foo-bar')).toBe('FooBarFooBar');
+    })
 })
